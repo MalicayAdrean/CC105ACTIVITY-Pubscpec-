d29c23498c8cefe85e44603a1751eee6830e8a0b
Adrean <bg2018.2925@wmsu.edu.ph>
2020-11-08 13:13:37 +0800

    FloatingActionButton

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Demo'),
+      ),
+      body: Center(
+        child: RawMaterialButton(
+          fillColor: Colors.red,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50.0),
+          ),
+            constraints: BoxConstraints.tightFor(height: 50.0, width: 100.0), onPressed: () {},
+        )
+      ),
+    );// This trailing comma makes auto-formatting nicer for build methods.
+  }
+}
